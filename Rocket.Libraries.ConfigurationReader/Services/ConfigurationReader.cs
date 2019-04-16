86524409534f6b0222f818069256ea6d3325ea79
Nyingi Maina <nyingimaina@gmail.com>
2019-04-16 12:25:17 +0300

    Code to read configs

---
+++ Rocket.Libraries.ConfigurationReader/Services/ConfigurationReader.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Rocket.Libraries.ConfigurationReader.Services
+{
+    public class ConfigurationReader
+    {
+        private IConfiguration ReadConfiguration()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var appSettingsGlobal = "appsettings.json";
+            var appSettingsEnv = $"appsettings.{environmentName}.json";
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile(appSettingsGlobal, optional: false, reloadOnChange: true)
+                .AddJsonFile(appSettingsEnv, optional: true);
+            return builder.Build();
+        }
+
+        public TResult Read<TResult>(string sectionName)
+        {
+            var configuration = ReadConfiguration();
+            return configuration.GetSection(sectionName).Get<TResult>();
+        }
+    }
+}
